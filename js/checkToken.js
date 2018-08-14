30e72dad8832f692d0da51ab0824f178d42bcb2e
gongyijie <2590911884@qq.com>
2018-08-15 00:57:52 +0800

    修改页面

--- js/checkToken.js
+++ js/checkToken.js
@@ -1,16 +1,16 @@
-// let login = localStorage.getItem("login");
-// let imei  = localStorage.getItem("imei");
-// if (temp && imei) {
-//     $.post( realmName + 'sf_zhzf/m/user/login',{
-//         login : login,
-//         imei  : imei
-//     },function(data,status){
-//         if(data.statusCode == 200){
-//             window.location.href = "./html/main.html";
-//         }else{
-//             alert(data.message);
-//         }
-//     });
-// }
+let login = localStorage.getItem("login");
+let imei  = localStorage.getItem("imei");
+if (temp && imei) {
+    $.post( realmName + 'sf_zhzf/m/user/login',{
+        login : login,
+        imei  : imei
+    },function(data,status){
+        if(data.statusCode == 200){
+            window.location.href = "./html/main.html";
+        }else{
+            alert(data.message);
+        }
+    });
+}
 
-window.location.href = "./html/main.html";
+// window.location.href = "./html/main.html";
